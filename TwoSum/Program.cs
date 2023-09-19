ab0a191f66001e1c7b737d1a5370ec6859d49bf3
Rennjay SOTERIO <Rennjay.SOTERIO@amadeus.com>
2023-09-19 18:37:14 +0800

    TwoSum Solution

---
+++ TwoSum/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        Console.WriteLine(Solution.TwoSum(nums, target).ToString());
+    }
+
+    public class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+
+                map[nums[i]] = i;
+            }
+            return new int[0];
+        }
+    }
+}
